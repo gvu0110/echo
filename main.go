881236358bed8b8215e749f456f5faf363298932
Giang Vu <gvu0110@gmail.com>
2020-03-22 19:22:29 -0400

    Query URL params and response JSON

--- main.go
+++ main.go
@@ -7,14 +7,39 @@ import (
 	"github.com/labstack/echo"
 )
 
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello from the Echo Web Server!")
+}
+
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catAge := c.QueryParam("age")
+
+	dataType := c.Param("data")
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Your cat's name is %s and cat's age is %s", catName, catAge))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"age":  catAge,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "Data type must be 'string' or 'json'",
+	})
+
+}
+
 func main() {
 	fmt.Println("Welcome to the Echo Web Server!")
 
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello from the Echo Web Server!")
-	})
+	e.GET("/", hello)
+	e.GET("/cats/:data", getCats)
 
 	e.Start(":8000")
 }
